20267f38fea33b3ccbba074f656c13320694da15
jamguimba <jamelahguimba@gmail.com>
2022-08-07 18:12:28 -0400

    1600 1.00E-03

--- pg-pong.py
+++ pg-pong.py
@@ -15,9 +15,9 @@ start = time.time()
 #end of insert 02082022
 
 # hyperparameters
-H = 200 # number of hidden layer neurons
+H = 1600 # number of hidden layer neurons
 batch_size = 10 # every how many episodes to do a param update?
-learning_rate = 1e-4
+learning_rate = 1e-3
 gamma = 0.99 # discount factor for reward
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
 resume = False # resume from previous checkpoint?
