4e0f5525afb5d8ec8980535efc7d2e9a445fed9d
jamguimba <jamelahguimba@gmail.com>
2022-08-07 18:01:27 -0400

    3200 1.00E-04

--- pg-pong.py
+++ pg-pong.py
@@ -15,7 +15,7 @@ start = time.time()
 #end of insert 02082022
 
 # hyperparameters
-H = 200 # number of hidden layer neurons
+H = 3200 # number of hidden layer neurons
 batch_size = 10 # every how many episodes to do a param update?
 learning_rate = 1e-4
 gamma = 0.99 # discount factor for reward
